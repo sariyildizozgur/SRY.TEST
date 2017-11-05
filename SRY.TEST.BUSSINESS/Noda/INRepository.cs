cb25d7ec48ad999db460626a8d1c415a38f93ae1
sariyildizozgur <sariyildizozgur@gmail.com>
2017-11-05 17:30:21 +0300

    Metodlar güncellendi.

--- SRY.TEST.BUSSINESS/Noda/INRepository.cs
+++ SRY.TEST.BUSSINESS/Noda/INRepository.cs
@@ -11,10 +11,10 @@ namespace SRY.TEST.BUSSINESS.Noda
     public interface INRepository<TEntity> where TEntity:IBaseEntity
     {
         IList<TEntity> GetAll();
-        void GetById(string id);
+        void Fill();
         IList<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
         TEntity Insert(TEntity entity);
-        void Update(TEntity entity);
-        void Delete(TEntity entity);
+        void Delete();
+        void Save();
     }
 }
--- SRY.TEST.BUSSINESS/Noda/NBase.cs
+++ SRY.TEST.BUSSINESS/Noda/NBase.cs
@@ -10,34 +10,129 @@ using SRY.TEST.DATA;
 
 namespace SRY.TEST.BUSSINESS.Noda
 {
-    public abstract class NBase<TEntity> : INRepository<TEntity> where TEntity: class, IBaseEntity
+    /// <summary>
+    /// Veri tabanı yönetimi için kullanılan base sınıfı
+    /// </summary>
+    /// <typeparam name="TEntity">Entity (Model tarafında karşılık gelen dbset)</typeparam>
+    public abstract class NBase<TEntity> : INRepository<TEntity> where TEntity : class, IBaseEntity
     {
+
+        #region Properties
+
+        /// <summary>
+        /// Temel Context
+        /// </summary>
         public TestContext Context { get; set; }
 
+        /// <summary>
+        /// İşlem sınıfı yapılacak tablo
+        /// </summary>
+        public TEntity Entity { get; set; }
+        #endregion
+
+        #region Const
+        
+        /// <summary>
+        /// Temel contex için tanımlama işlemi yapar.
+        /// </summary>
         protected NBase()
         {
             Context = new TestContext();
         }
 
-        public TEntity Entity { get; set; }
+        #endregion
 
+        #region Metod
+
+        /// <summary>
+        /// Bütün kayıtları getirme işlemi yapar.
+        /// </summary>
+        /// <returns></returns>
         public IList<TEntity> GetAll()
         {
-            var list = Context.Set<TEntity>();
-            return list.ToList();
+            try
+            {
+                var list = Context.Set<TEntity>();
+                return list.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
-        public void GetById(string id)
+
+        /// <summary>
+        /// Kayıt doldurma işlemi yapar.
+        /// </summary>
+        public void Fill()
         {
-            Entity = Context.Set<TEntity>().FirstOrDefault(p => p.Id == id);
+            try
+            {
+                if (!string.IsNullOrEmpty(Entity.Id))
+                    throw new Exception("Kayıt getirme işlemi için id tanımlı olmalıdır.");
+
+                Entity = Context.Set<TEntity>().FirstOrDefault(p => p.Id == Entity.Id);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
         }
 
+        /// <summary>
+        /// Kayıt işlemi gerçekleştirir.
+        /// </summary>
+        public virtual void Save()
+        {
+
+            try
+            {
+                if (!string.IsNullOrEmpty(Entity.Id))
+                {
+                    Entity.Id = Guid.NewGuid().ToString();
+                    Context.Set<TEntity>().Add(Entity);
+                }
+                else
+                {
+                    Context.Entry(Entity).State = EntityState.Modified;
+                }
+
+                Context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// Sorgulama işlemi gerçekleştirir.
+        /// </summary>
+        /// <param name="predicate">Sorgulama parametresi(LINQ)</param>
+        /// <returns></returns>
         public IList<TEntity> Where(Expression<Func<TEntity, bool>> predicate)
         {
-            var result = Context.Set<TEntity>().Where(predicate);
-            return result.ToList();
+            try
+            {
+                var result = Context.Set<TEntity>().Where(predicate);
+                return result.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
+
+        /// <summary>
+        /// Kayıt ekleme işlemi yapar.(Ekleme sonrası kayıt işlemi gerçekleştirmez.)
+        /// </summary>
+        /// <param name="entity">Entity objesi</param>
+        /// <returns></returns>
         public TEntity Insert(TEntity entity)
         {
             try
@@ -47,25 +142,57 @@ namespace SRY.TEST.BUSSINESS.Noda
             }
             catch (Exception ex)
             {
-                return null;
+                throw new Exception(ex.Message);
             }
         }
 
-        public void Update(TEntity entity)
+        /// <summary>
+        /// Kayıt güncellendi mi durum bilgisini verir.
+        /// </summary>
+        /// <param name="entity"></param>
+        public bool IsUpdate(TEntity entity)
         {
-            Context.Entry(entity).State = EntityState.Modified;
+            return Context.Entry(entity).State == EntityState.Modified;
         }
 
-        public void Delete(TEntity entity)
+        /// <summary>
+        /// Kayıt silme işlemi gerçekleştirir.
+        /// </summary>
+        public void Delete()
         {
-            Context.Set<TEntity>().Remove(entity);
+            try
+            {
+                //todo:kayıt direk silme yerine kayıt iptal durumu ileride güncelleme yapılacak.
+                if (string.IsNullOrEmpty(Entity.Id))
+                    throw new Exception("Silinecek tablo tanımı dolu değil.");
+
+                Context.Set<TEntity>().Remove(Entity);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
+        /// <summary>
+        /// Genel değişikler için kayıt işlemi yapar.
+        /// </summary>
+        /// <returns></returns>
         public virtual int SaveChanges()
         {
-            return Context.SaveChanges();
+            try
+            {
+                return Context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
+        #endregion
+
+
     }
-   
+
 }
